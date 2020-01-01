43dc57d0547f2e61cd8c54a042802deb76964eb0
sasuseso <sinai471530@gmail.com>
2020-01-01 13:34:58 +0900

    added options

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # DOTMAN - a simple dotfiles manager
 ## config.toml format
+can parse TOML v0.4.0
 ### tables
+#### [repo]
+your github repository that dotfiles are there
 #### [[dotfile]]
 represent a file.  
 has following keys.
 - name(String)
   - filename
 - path([String, String])
-  - path whare file. [original file, symlink]
+  - path whare file. relative path from cloned repo root. [original file, symlink]
 - hook(String)
-  - the shell command that you want to execute after deployed the file.
+  - the shell command that you want to execute after deployed the file. you can use the keyword "{_*_FILE_*_}". it's will replace with the filename.
 - sofware(String)
   - software that uses the file
-
--- src/DotfilesManager.jl
+++ src/DotfilesManager.jl
@@ -1,18 +1,44 @@
 module DotfilesManager
+using TOML, ArgParse
 
 function main()
+								config_path = "./config.toml"
+								isfile(config_path) ? config = TOML.parsefile(config_path) : error("config file path invalid") 
+
+								symlink_dotfiles(TOML.parse(config))
 end
 
 function detect_distro()
 end
 
-function symlink_dotfiles()
+function symlink_dotfiles(config::Dict)
 								cp(".config", joinpath(homedir(), ".config"); force=true)
 								for i in readdir(".")
 																if isfile(i)
 																								symlink(joinpath(pwd(), i), joinpath(homedir(), i))
 																end
 								end
+end
+
+function parse_opts(args::Vector{String})
+								s = ArgParseSettings("DOTMAN - Dotfiles Managemer")
+
+								@add_arg_table s begin
+																"--help", "-h"
+																								help = "Show this help."
+																								action = :show_help
+																
+																"--version", "-v"
+																								help = "Show version."
+																								action = :show_version
+
+																"--config-file", "-c"
+																								help = "Specifies config.toml to use."
+																								arg_type = String
+																								default = "./config.toml"
+								end
+
+								parse_args(args, s)
 end
 
 end # module
